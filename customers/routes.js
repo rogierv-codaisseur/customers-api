fb2dc920ba7be2b29a7b384f3a98e539e40c3adf
Rogier Verkaik <rogierv@gmail.com>
2019-02-05 17:18:40 +0100

    initial commit

--- customers/routes.js
+++ customers/routes.js
@@ -1,74 +1,70 @@
-const { Router } = require('express')
-const Customer = require('./model')
+const { Router } = require('express');
+const Customer = require('./model');
 
-const router = new Router()
+const router = new Router();
 
 router.get('/customers', (req, res, next) => {
-  Customer
-    .findAll()
+  Customer.findAll()
     .then(customers => {
-      res.send({ customers })
+      res.send({ customers });
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 router.get('/customers/:id', (req, res, next) => {
-  Customer
-    .findById(req.params.id)
+  Customer.findById(req.params.id)
     .then(customer => {
       if (!customer) {
         return res.status(404).send({
           message: `Customer does not exist`
-        })
+        });
       }
-      return res.send(customer)
+      return res.send(customer);
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 router.post('/customers', (req, res, next) => {
-  Customer
-    .create(req.body)
+  Customer.create(req.body)
     .then(customer => {
       if (!customer) {
         return res.status(404).send({
           message: `Customer does not exist`
-        })
+        });
       }
-      return res.status(201).send(customer)
+      return res.status(201).send(customer);
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 router.put('/customers/:id', (req, res, next) => {
-  Customer
-    .findById(req.params.id)
+  Customer.findById(req.params.id)
     .then(customer => {
       if (!customer) {
         return res.status(404).send({
           message: `Customer does not exist`
-        })
+        });
       }
-      return customer.update(req.body).then(customer => res.send(customer))
+      return customer.update(req.body).then(customer => res.send(customer));
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 router.delete('/customers/:id', (req, res, next) => {
-  Customer
-    .findById(req.params.id)
+  Customer.findById(req.params.id)
     .then(customer => {
       if (!customer) {
         return res.status(404).send({
           message: `Customer does not exist`
-        })
+        });
       }
-      return customer.destroy()
-        .then(() => res.send({
+      return customer.destroy().then(() =>
+        res.send({
           message: `Customer was deleted`
-        }))
+        })
+      );
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
-module.exports = router
+module.exports = router;
