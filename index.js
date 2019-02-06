661019633dd26909dd65fc6d8be9d52463339c7b
Rogier Verkaik <rogierv@gmail.com>
2019-02-06 12:17:21 +0100

    Add signup endpoint

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const authRouter = require('./auth/routes');
 const customersRouter = require('./customers/routes');
 const companiesRouter = require('./companies/routes');
+const usersRouter = require('./users/routes');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,6 +12,7 @@ const port = process.env.PORT || 4000;
 app
   .use(bodyParser.json())
   .use(authRouter)
-  .use(customersRouter)
   .use(companiesRouter)
+  .use(customersRouter)
+  .use(usersRouter)
   .listen(port, () => console.log(`Listening on port ${port}`));
