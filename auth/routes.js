7bf43b3fef0f5f788585d5d2a247fb01a3d67025
Rogier Verkaik <rogierv@gmail.com>
2019-02-06 11:32:12 +0100

    Add secret endpoint

--- auth/routes.js
+++ auth/routes.js
@@ -1,16 +1,38 @@
 const { Router } = require('express');
 
-const { toJWT } = require('./jwt');
+const { toJWT, toData } = require('./jwt');
 
 const router = new Router();
 
-router.post('/logins', (req, res, next) => {
+router.post('/logins', (req, res) => {
   const { email, password } = req.body;
   if (email && password) {
     res.send({ jwt: toJWT({ userId: 1 }) });
   } else {
     res.status(400).send({
       message: 'Please supply a valid email and password'
+    });
+  }
+});
+
+router.get('/secret-endpoint', (req, res) => {
+  const auth =
+    req.headers.authorization && req.headers.authorization.split(' ');
+  if (auth && auth[0] === 'Bearer' && auth[1]) {
+    try {
+      const data = toData(auth[1]);
+      res.send({
+        message: 'Thanks for visiting the secret endpoint.',
+        data
+      });
+    } catch (error) {
+      res.status(400).send({
+        message: `Error ${error.name}: ${error.message}`
+      });
+    }
+  } else {
+    res.status(401).send({
+      message: 'Please supply some valid credentials'
     });
   }
 });
